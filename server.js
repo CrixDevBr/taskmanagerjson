07c4ef71c5005f219749290286cf9146d245bd65
CrixKyrios <crisbelarma@gmail.com>
2025-10-24 13:18:48 -0300

    Set up JSON server with CORS support

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+server.use(router);
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => console.log(`API rodando na porta ${port}`));
